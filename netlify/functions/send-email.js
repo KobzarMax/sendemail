696f7f223cf4cd92890177a67362810e207d6e24
Max Kobzar <nimbus2219@gmail.com>
2024-11-11 20:40:33 +0100

    with sendgrid

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -2,17 +2,9 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.handler = async (event) => {
-  // Ensure this is a POST request
-  if (event.httpMethod !== 'POST') {
-    return {
-      statusCode: 405,
-      body: JSON.stringify({ message: 'Method Not Allowed' }),
-    };
-  }
-
   const allowedOrigin = "https://lesenokbags-ua.webflow.io";
 
-  // Check the request method and respond to preflight requests
+  // Handle preflight OPTIONS request
   if (event.httpMethod === "OPTIONS") {
     return {
       statusCode: 200,
@@ -25,26 +17,37 @@ exports.handler = async (event) => {
     };
   }
 
+  // Ensure this is a POST request
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      headers: {
+        "Access-Control-Allow-Origin": allowedOrigin,
+      },
+      body: JSON.stringify({ message: "Method Not Allowed" }),
+    };
+  }
+
   try {
     // Parse the request body
     const { email, name, surname, orderNumber, formattedTotalPrice, formattedOrderItems } = JSON.parse(event.body);
 
     // Construct the email message
     const msg = {
-        personalizations: [
-          {
-            to: [{ email }],
-            dynamic_template_data: {
-              first_name: `${name} ${surname}`,
-              order_id: orderNumber,
-              total_price: formattedTotalPrice,
-              order_items: formattedOrderItems,
-            },
+      personalizations: [
+        {
+          to: [{ email }],
+          dynamic_template_data: {
+            first_name: `${name} ${surname}`,
+            order_id: orderNumber,
+            total_price: formattedTotalPrice,
+            order_items: formattedOrderItems,
           },
-        ],
-        from: { email: 'lesenokbags@gmail.com' },
-        template_id: 'd-94b1142e7c0e4e9dbacb9cb7ce646514', // Use your actual SendGrid template ID
-      };
+        },
+      ],
+      from: { email: 'lesenokbags@gmail.com' },
+      template_id: 'd-94b1142e7c0e4e9dbacb9cb7ce646514', // Replace with your actual SendGrid template ID
+    };
 
     // Send the email
     await sgMail.send(msg);
@@ -52,14 +55,20 @@ exports.handler = async (event) => {
     // Return a success response
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": allowedOrigin,
+      },
       body: JSON.stringify({ message: 'Email sent successfully!' }),
     };
   } catch (error) {
-    console.error(error);
+    console.error("Error sending email:", error);
 
     // Return an error response
     return {
       statusCode: error.code || 500,
+      headers: {
+        "Access-Control-Allow-Origin": allowedOrigin,
+      },
       body: JSON.stringify({ error: error.message || 'Failed to send email' }),
     };
   }
