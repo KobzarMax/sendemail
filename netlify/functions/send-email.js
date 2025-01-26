938b79fcaf29ab9262211304f7a0f7c4b580d1c5
Max Kobzar <nimbus2219@gmail.com>
2025-01-26 19:14:02 +0000

    update body

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,87 +1,3 @@
-// const sgMail = require('@sendgrid/mail');
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// exports.handler = async (event) => {
-//   const allowedOrigin = "";
-
-//   // Handle preflight OPTIONS request
-//   if (event.httpMethod === "OPTIONS") {
-//     return {
-//       statusCode: 200,
-//       headers: {
-//         "Access-Control-Allow-Origin": allowedOrigin,
-//         "Access-Control-Allow-Methods": "POST, OPTIONS",
-//         "Access-Control-Allow-Headers": "Content-Type",
-//       },
-//       body: JSON.stringify({ message: "CORS preflight response" }),
-//     };
-//   }
-
-//   // Ensure this is a POST request
-//   if (event.httpMethod !== "POST") {
-//     return {
-//       statusCode: 405,
-//       headers: {
-//         "Access-Control-Allow-Origin": allowedOrigin,
-//       },
-//       body: JSON.stringify({ message: "Method Not Allowed" }),
-//     };
-//   }
-
-//   try {
-//     // Parse and validate the request body
-//     const { email, name, surname, orderNumber, formattedTotalPrice, formattedOrderItems } = JSON.parse(event.body);
-
-//     // Convert formattedOrderItems to a single formatted string
-    
-
-//     // Construct the email message
-//     const msg = {
-//       personalizations: [
-//         {
-//           to: [{ email }],
-//           dynamic_template_data: {
-//             first_name: `${name} ${surname}`,
-//             order_id: orderNumber.toString(),
-//             total_price: formattedTotalPrice,
-//             order_items: orderItemsString, // Pass as formatted string
-//           },
-//         },
-//       ],
-//       from: { email: 'lesenokbags@gmail.com' },
-//       template_id: process.env.SENDGRID_TEMPLATE_ID,
-//     };
-
-//     // Send the email
-//     await sgMail.send(msg);
-
-//     // Return a success response
-//     return {
-//       statusCode: 200,
-//       headers: {
-//         "Access-Control-Allow-Origin": allowedOrigin,
-//       },
-//       body: JSON.stringify({ message: 'Email sent successfully!' }),
-//     };
-//   } catch (error) {
-//     console.error("Error sending email:", {
-//       message: error.message,
-//       code: error.code,
-//       response: error.response && error.response.body
-//     });
-
-//     // Return an error response
-//     return {
-//       statusCode: error.code || 500,
-//       headers: {
-//         "Access-Control-Allow-Origin": allowedOrigin,
-//       },
-//       body: JSON.stringify({ error: error.message || 'Failed to send email' }),
-//     };
-//   }
-// };
-
-
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
@@ -116,7 +32,7 @@ exports.handler = async (event) => {
 
   try {
     // Parse the request body
-    const { email, name, surname, orderNumber, formattedTotalPrice, formattedOrderItems } = JSON.parse(event.body);
+    const { email, name, surname, orderNumber, formattedTotalPrice, formattedOrderItems, comments } = JSON.parse(event.body);
 
     const orderItemsString = formattedOrderItems
       .map(item => `• ${item.itemName}, - ${item.itemQuantity} x ${item.itemPrice}`)
@@ -130,6 +46,7 @@ exports.handler = async (event) => {
           dynamic_template_data: {
             first_name: `${name} ${surname}`,
             order_id: orderNumber,
+            comments: comments,
             total_price: formattedTotalPrice,
             order_items: orderItemsString,
           },
